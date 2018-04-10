df6ecace25fcc8006ab5c0e04e293f6fba6c1d4e
David Ambrose Wehr - student <dawehr@hpc-class.its.iastate.edu>
2018-04-10 10:29:53 -0500

    Slurm scripts

---
+++ execute4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=1:00:00
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=1
+#SBATCH --job-name="kmeans"
+#SBATCH --output="kmeans_data_n4.out"
+module load intel
+for i in {1..10}
+do
+    for k in 2 4 6 8 10 20
+    do
+        mpirun david_impl/kmeans 1000000 $k y
+    done
+done
